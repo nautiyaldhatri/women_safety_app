80b5159eb5e49a05e7f03f5beecba892679c69b5
hansika-cse <2k25cse2510453@gmail.com>
2026-09-16 21:45:50 +0530

    Integrate TFLite model loading in main.dart
    Added TFLite model loading functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,13 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'login_screen.dart';
 import 'vosk_test_screen.dart';
 import 'app_home_listener_screen.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+
+late Interpreter interpreter;
+
+Future loadModel() async {
+  interpreter = await Interpreter.fromAsset('assets/models/model.tflite');
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
